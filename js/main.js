28e513ce87bcfb3b27cb923bcbf10666162d8b45
Roemer Naber <200611@student.glu.nl>
2021-03-09 14:23:15 +0100

    les 2 deel1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 console.log("start2");
 let boardsize = 12;
 let board = document.getElementById("board");
+let keyboardInput = document.getElementById("keyboardInput");
+let direction = 0;
+
+let position = 
+
 
 function drawBoard() {
     for (let j = 0; j < boardsize; j++) {
@@ -18,3 +23,26 @@ let halfBoardSize = Math.round(boardsize/2)-1;
 let snakeHeadPosition = "x"+halfBoardSize+"y"+halfBoardSize;
 console.log(snakeHeadPosition);
 let head = document.getElementById(snakeHeadPosition).style.background="red";
+
+
+window.addEventListener("keydown", function(event){
+    keyboardInput.innerHTML = event.key;
+    if (event.key == "ArrowUp")
+    {
+        keyboardInput.innerHTML = "up";
+        direction = "up";
+    }
+    if (event.key == "ArrowDown")
+    {
+        keyboardInput.innerHTML = "down";
+    }
+    if (event.key == "ArrowRight")
+    {
+        keyboardInput.innerHTML = "right";
+    }
+    if (event.key == "ArrowLeft")
+    {
+        
+    }
+    keyboardInput.innerHTML = direction;
+}, true);
